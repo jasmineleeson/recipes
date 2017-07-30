0d795b48600a283e4790d27b11cac26fb499783b
Jasmine <jasmine.leeson@gmail.com>
2017-07-30 14:37:23 -0400

    add react router and base components

--- app/javascript/components/home/base.js
+++ app/javascript/components/home/base.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import Routes from './routes'
+import { Link } from 'react-router-dom'
 
-export default function Hello (props) {
-  return <div>HI!!!</div>
+export default function HomeBase () {
+  return (
+    <div>
+      <div className="small-12 medium-9 small-centered columns">
+        <div className="jasmine">
+          <Link to={`/`}>Family Profile</Link>
+          <Link to={`/family-recipes`}>Family Recipes</Link>
+          <Routes />
+        </div>
+      </div>
+    </div>
+  )
 }
-
-// const Hello = props => (
-//   <div>Hello {props.name}!</div>
-// )
-
-// Hello.defaultProps = {
-//   name: 'David'
-// }
-
-// Hello.propTypes = {
-//   name: PropTypes.string
-// }
