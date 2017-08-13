40766edd563e6839c47fc795573aa169e184486c
Jasmine <jasmine.leeson@gmail.com>
2017-08-13 19:42:56 -0400

    add no family page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,10 +2,13 @@ class RootController < ApplicationController
   def home
     if logged_in?
       user = current_user
-      family = user.family
-      recipes = family.recipes
-      @home_facade = HomeFacade.new(user, family, recipes)
-      render 'signed_in'
+      if family = user.family
+        recipes = family.recipes
+        @home_facade = HomeFacade.new(user, family, recipes)
+        render 'signed_in'
+      else
+        render 'no_family'
+      end
     else
       render 'signed_out'
     end
